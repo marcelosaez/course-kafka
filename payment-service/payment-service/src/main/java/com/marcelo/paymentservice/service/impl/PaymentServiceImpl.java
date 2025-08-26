794f5a3a3747c14bc3ddf1713d80921e0ce6fa0c
Marcelo Saez <marcelo.saez@outlook.com>
2025-08-26 11:48:30 -0300

    feat: Implement JsonProducerConfig and enhance PaymentServiceImpl for Kafka integration

--- payment-service/payment-service/src/main/java/com/marcelo/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/payment-service/src/main/java/com/marcelo/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,29 @@ package com.marcelo.paymentservice.service.impl;
 
 import com.marcelo.paymentservice.model.Payment;
 import com.marcelo.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT SERVICE RECEIVED {}", payment);
+        log.info("Pagamento recebido {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento... {}", payment);
+        kafkaTemplate.send("payment-topic", payment);
 
     }
 }
